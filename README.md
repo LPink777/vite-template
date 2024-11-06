c23b5093452f083aadbb1915498ad0a9454af251
LPink777 <liangfenfen@live.com>
2024-11-06 23:57:32 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# vite-template
+vite 模版
+xxx
+# vite-template
+# vite-template
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./app.scss";
+
+// 输出一个计算器的样式组件 可以使用计算器进行加减操作
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center text-[28px] font-bold">
+        vite-template
+      </div>
+    </>
+  );
+}
+
+export default App;
